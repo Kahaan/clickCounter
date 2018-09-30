2c99e3b0027a7e6f2a7b0c3083348fb0a1d22375
Kahaan <kahaan.r.patel@gmail.com>
2018-09-30 13:39:41 -0700

    react..

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
-import Abc from './Abc'
+// import Abc from './Abc'
 
 class App extends Component {
   constructor(){
     super()
-    this.state = {count:1}
+    this.state = {count:1, previous:[]}
     this.click = this.click.bind(this)
+    this.reset = this.reset.bind(this)
   }
 
   click(event){
@@ -15,11 +16,19 @@ class App extends Component {
     this.setState({count: this.state.count + 2})
   }
 
+  reset(event){
+    event.preventDefault()
+    const previous = this.state.previous
+    previous.push(this.state.count)
+    this.setState({count: 0, previous:previous})
+  }
+
   render() {
     return (
       <div className="App">
         <button onClick={this.click}>CLick me!</button>
-        {this.state.count}
+        <button onClick={this.reset}>Reset</button>
+        <p className="p">{this.state.count}</p>
       </div>
     );
   }
